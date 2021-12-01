efa98e8ec2e873f8d58ec17b60eff1e29ec4a3ab
Anubi5555 <dragobegovic555@gmail.com>
2021-12-01 23:50:07 +0100

    recepti

--- app-docker/front/static/lib/js/bundle.js
+++ app-docker/front/static/lib/js/bundle.js
@@ -425,7 +425,7 @@
 	 function(module, exports) {
 
 	
-	var recipes = [["dough", ["flour", "water"]],["mayo", ["egg", "egg"]],["bread", ["dough", "salt"]],["sugar-cubes", ["sugar-cube", "sugar-cube"]],["cheese", ["milk", "milk"]],["cake", ["cheescake", "chocolate"]],["cherry-cheescake", ["cherry", "cheescake"]],["strawberry-cheescake", ["cheescake", "strawberry"]],["hamburger", ["steak", "bread"]],["waffle", ["bread", "egg"]],["sauce", ["tomato", "tomato"]],["salad", ["lettuce", "lettuce"]],["salad", ["tomato", "lettuce"]],["sushi", ["fish", "rice"]],["cookie", ["dough", "chocolate"]],["cupcake", ["cookie", "chocolate"]],["gingerbread-house", ["cookie", "cookie"]],["rice-cake", ["rice", "sugar-cube"]], ["rice-cake", ["rice", "cheescake"]],["bento", ["sushi", "sushi"]],["birthday-cake", ["cake", "cupcake"]],["pizza",["sauce","cheese"]],["cheescake", ["dough", "sugar-cube"]]];
+		var recipes = [["dough", ["flour", "water"]],["mayo", ["egg", "egg"]],["bread", ["dough", "salt"]],["sugar-cubes", ["sugar-cube", "sugar-cube"]],["cheese", ["milk", "milk"]],["hamburger", ["steak", "bread"]],["waffle", ["bread", "egg"]],["sauce", ["tomato", "tomato"]],["salad", ["lettuce", "lettuce"]],["salad", ["tomato", "lettuce"]],["sushi", ["fish", "rice"]],["cookie", ["dough", "chocolate"]],["cupcake", ["cookie", "chocolate"]],["gingerbread-house", ["cookie", "cookie"]],["rice-cake", ["rice", "sugar-cube"]], ["rice-cake", ["rice", "cheescake"]],["bento", ["sushi", "sushi"]],["birthday-cake", ["cake", "cupcake"]],["pizza",["sauce","cheese"]],["cheescake", ["dough", "sugar-cube"]],["cake", ["cheescake", "chocolate"]],["cherry-cheescake", ["cherry", "cheescake"]],["strawberry-cheescake", ["cheescake", "strawberry"]]];
 
 
 	var allRecipes = recipes.reduce((comb, [first, second]) => {
