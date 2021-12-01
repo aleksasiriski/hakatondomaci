91b4a934fcb619fdc9c9416d6c284a69469cdfef
Elena Eraković <elena.erakovic@gmail.com>
2021-12-01 18:39:35 +0100

    views

--- app-docker/back/views/user.js
+++ app-docker/back/views/user.js
@@ -1,18 +1,19 @@
-// includes
 const { Router } = require("express")
 const router = Router()
 const check = require("../controller/authentication")
 
-// users
+
 router.get("/login", check.isNotAuthenticated, (req, res) => {
     res.render("../front/views/login.ejs")
 })
+
 router.get("/register", check.isNotAuthenticated, (req, res) => {
     res.render("../front/views/register.ejs")
 })
+
 router.get("/profile", check.isAuthenticated, (req, res) => {
     res.render("../front/views/profile.ejs")
 })
 
-// export
+
 module.exports = router
