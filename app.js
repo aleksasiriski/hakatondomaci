e084d53e18512921ba7e11625fe10448d9b55853
aleksasiriski <aleksasiriski@lavelektronik.com>
2021-12-01 15:00:33 +0100

    Dockerfile and docker-compose

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const chatRoute = require("./back/controller/chat")
 app.use("/api", chatRoute)
 
 // startup
-const port = 3000
+const port = process.env.PORT
 app.listen(port, () => {
     console.log("Listening on port: " + port)
 })
